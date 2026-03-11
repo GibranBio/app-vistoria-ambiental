f1f74be053f20d6b1ae685b4b1a7ccc8a0b4d129
GibranBio <gibransoaambiental@gmail.com>
2026-03-11 16:35:12 -0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="App Vistoria Ambiental", layout="wide")
-st.title("App Vistoria Ambiental")
-st.write("Deploy OK ✅ Agora vamos ligar o template Excel e a tabela.")
