c2a2cb04d578b1751dfc576da1aacb3526c196ad
GibranBio <gibransoaambiental@gmail.com>
2026-03-11 15:56:46 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="App Vistoria Ambiental", layout="wide")
+st.title("App Vistoria Ambiental")
+st.write("Deploy OK ✅ Agora vamos ligar o template Excel e a tabela.")
